0519ff44f6cb50e5f4d10c3827425d24e00e46cb
Keilane Rizza <keilanerizza@gmail.com>
2018-02-15 23:09:16 -0200

    Add files via upload

---
+++ aula0802/ex3.java
@@ -0,0 +1,21 @@
+//1.3. (atributo) Escreva uma classe para representar um paciente com atributos para armazenar nome, peso e altura.
+package aula0802;
+
+public class ex3 {
+    
+    public static void main(String[] args){
+        
+    }
+}
+
+class Paciente {
+    String nome;
+    double peso;
+    double altura;
+    
+    // Assinatura double calcularIMC()
+    // Efeito Retorna o índice de massa corpórea do paciente usando a fórmula: peso/altura2
+    double calcularIMC() {
+        
+    }
+}
